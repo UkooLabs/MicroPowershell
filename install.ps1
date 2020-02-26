ffbab677f677db36f5281d12adf9d09879d15c43
Peter Tribe <58004848+UkooLabs@users.noreply.github.com>
2020-02-26 12:07:23 -0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,2 +1,13 @@
-curl https://github.com/UkooLabs/MicroPowershell/blob/master/micro.exe?raw=true -outfile micro.exe
-curl https://github.com/UkooLabs/MicroPowershell/blob/master/settings.json?raw=true -outfile settings.json
+$installPath = (Join-Path $env:ProgramData "micro")
+
+New-Item -ItemType Directory -Force -Path $installPath
+
+$files = @("micro.exe", "settings.json")
+foreach ($file in $files) 
+{
+  curl https://github.com/UkooLabs/MicroPowershell/blob/master/$file?raw=true -outfile (Join-Path $installPath $file)
+}
+
+$regexInstallPath = [regex]::Escape($installPath)
+$pathArray = $env:Path -split ';' | Where-Object {$_ -notMatch "^$regexInstallPath\\?"}
+$env:Path = ($pathArray + $installPath) -join ';'
