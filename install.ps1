bdd8af7481894a1acf94524902851bff22893c68
Peter Tribe <58004848+UkooLabs@users.noreply.github.com>
2020-02-26 11:31:40 -0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,2 @@
 curl https://github.com/UkooLabs/MicroPowershell/blob/master/micro.exe?raw=true -outfile micro.exe
+curl https://github.com/UkooLabs/MicroPowershell/blob/master/settings.json?raw=true -outfile settings.json
