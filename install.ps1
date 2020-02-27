7f28727e709ef0578ed0720340ae12270cd59085
Peter Tribe <58004848+UkooLabs@users.noreply.github.com>
2020-02-26 18:16:28 -0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ New-Item -ItemType Directory -Force -Path $installPath
 $files = @("micro.exe", "settings.json")
 foreach ($file in $files) 
 {
-  $url = "https://github.com/UkooLabs/MicroPowershell/blob/master/$file?raw=true" 
+  $url = 'https://github.com/UkooLabs/MicroPowershell/blob/master/' + $file + '?raw=true' 
   Write-Output "$url"
   $outFile = (Join-Path $installPath $file)
   curl $url -outfile $outFile
